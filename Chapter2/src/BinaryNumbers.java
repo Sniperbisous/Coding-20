f5f1509699239b86e375663f79712d0f43dafbb5
105267002 <105267002@L815WD150053.edu.cbe.ab.ca>
2022-02-23 09:58:11 -0700

    Chapter 2 Projects

---
+++ Chapter2/src/BinaryNumbers.java
@@ -0,0 +1,30 @@
+/*
+
+Program: ????.java          Last Date of this Revision: September 30, 2019
+
+Purpose: Displays 1-20 in binary.
+
+Author: Aiden Storer 
+School: CHHS
+Course: Computer Programming 20
+*/
+
+public class BinaryNumbers 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		
+		System.out.println();  
+		System.out.println(); //1 + "/t" ___ /n	+ 2 + "/t" ___ /n + 3 + "/t" ___ /n	+ 4 + "/t" ___ /n	+ 5 + "/t" ___ /n	+ 6 + "/t" ___ /n
+		  
+	}
+	
+}
+
+/* Screen Dump
+
+Paste the output of your code here
+ 
+ */
---
+++ Chapter2/src/TicTacToe.java
@@ -0,0 +1,47 @@
+/*
+
+Program: TicTacToe.java          Last Date of this Revision: Feb 14, 2022
+
+Purpose: Displays a Tic Tac Toe table.
+
+Author: Aiden Storer 
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
+public class TicTacToe
+{
+
+	public static void main(String[] args)
+	{
+		// Displays The X and O grid as well as the middle X
+		System.out.println("		|		|		");
+		System.out.println("		|		|		");	
+		System.out.println("		|		|		");
+		System.out.println("	-------------------------------		");
+		System.out.println("		|		|		");
+		System.out.println("		|	X	|		");
+		System.out.println("		|		|		");
+		System.out.println("	-------------------------------		");
+		System.out.println("		|		|		");
+		System.out.println("		|		|		");	
+		System.out.println("		|		|		");
+	}
+
+}
+/* Screen Dump
+
+		|		|		
+		|		|		
+		|		|		
+-------------------------------		
+		|		|		
+		|	X	|		
+		|		|		
+-------------------------------		
+		|		|		
+		|		|		
+		|		|		
+
+ */
