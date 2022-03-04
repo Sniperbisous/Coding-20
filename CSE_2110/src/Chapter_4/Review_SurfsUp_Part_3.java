0fe76e994bf2f9174ebf607e13126c0358153fea
105267002 <105267002@L815WD150053.edu.cbe.ab.ca>
2022-03-04 10:09:32 -0700

    Done (March 4, 2022)

--- CSE_2110/src/Chapter_4/Review_SurfsUp_Part_3.java
+++ CSE_2110/src/Chapter_4/Review_SurfsUp_Part_3.java
@@ -1,8 +1,9 @@
 /*
 
-Program: Review_SurfsUp_Part_3.java          Last Date of this Revision: March 3, 2022
+Program: Review_SurfsUp_Part_3.java          Last Date of this Revision: March 4, 2022
 
-Purpose: To display "Great day for surfing!" when the input wave high is higher than 6, "Go body boarding!" when it is less than 6 and higher than 3 and "Whoa! What kind of surf is that?" 
+Purpose: To display "Great day for surfing!" when the input wave high is higher than 6, "Go body boarding!" when it is less than 6 and higher than 3, "Go for a swim." when it is less than 
+3 but above 0 and "Whoa! What kind of surf is that?" if all other conditions prove false.
 when it is less than 3.
 
 Author: Aiden Storer 
@@ -38,14 +39,14 @@ public class Review_SurfsUp_Part_3
 				System.out.println("Great day for surfing!");
 			}
 				
-		//if it is false it will then check to see the wave height and if it is between 3-6 it will display "Go body boarding!" if true
+		//If it is false it will then check to see the wave height and if it is between 3-6 it will display "Go body boarding!" if true
 		else if(wave_h >= 3 && wave_h < 6)  
 			{
 				System.out.println("Go body boarding!");
 			}
 				
-		//If the wave height is less than 3 "Go for a swim." 
-		else if(wave_h < 3)
+		//If the wave height is less than 3 but higher than 0 it displays "Go for a swim." 
+		else if(wave_h < 3 && wave_h > 0)
 			{
 				System.out.println("Go for a swim.");
 			}
@@ -68,6 +69,11 @@ Great day for surfing!
 How high is your wave in feet:
 4
 Go body boarding!
+
+
+How high is your wave in feet:
+2
+Go for a swim.
 
 
 How high is your wave in feet:
