215b5ca8f22e302c231457bf3e4a7a75de27f8ac
105267002 <105267002@L815WD150053.edu.cbe.ab.ca>
2022-03-14 09:58:59 -0600

    Done(March 14, 2022)

---
+++ CSE_2110/src/Chapter_4/CRT_Chapter_4.java
@@ -0,0 +1,85 @@
+/* 
+ 1.	
+ 		A. 	if (Grade >= 90)
+			{				
+			System.out.println(" Good job! ");	
+			}
+
+  		B. if (number < 20 && number > 50)
+			{				
+			System.out.println(" Error ");	
+			}
+			
+		C.	if (y < 100)
+			{				
+				Int y = y + 2;	
+			}
+
+ 
+ 2. 
+  	If (num1 > num2)
+		{
+			System.out.println(“First number is larger.”);
+		}
+
+ 	Else if (num1 < num2)
+		{
+			System.out.println(“Second number is larger.”);
+		}
+	
+	Else 
+		{
+		System.out.println(“Numbers are equal.”);
+		}
+
+
+ 
+3. 
+	A. 
+		If (num % 2 == 0)
+			{
+
+			System.out.println(“even number”);
+		
+			}
+ 		else 
+			{
+
+			System.out.println(“odd number”);
+
+			}
+
+ 	B. 
+ 		Int case = num % 2 
+ 
+		switch(case)
+
+			Case 0
+ 			{
+				System.out.println(“even number”);
+			}
+
+			Case 1
+			{
+				System.out.println(“odd number”);
+			}
+
+
+
+4. 
+	A. 	rand_n1 = (int)(50 * Math.random()) + 1; 
+	B.	rand_n1 = (int)(100 * Math.random()) + 20;
+ 	C.	rand_n1 = (10 * Math.random()) + 10; 
+
+5. 	
+	It should be 18 <= age in order to display a message for the age of 18 
+
+6. 
+ 	A. True
+ 	B. False
+ 	C. True
+ 	D. True
+ 	E. True
+ 	F. True
+ 	G. True
+*/
