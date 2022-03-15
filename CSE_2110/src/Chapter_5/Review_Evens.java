feeeabd76ccb07a5a1249d4ae66b7a83bf34bcf0
105267002 <105267002@L815WD150053.edu.cbe.ab.ca>
2022-03-15 11:57:20 -0600

    Done (March 15, 2022)

--- CSE_2110/src/Chapter_5/Review_Evens.java
+++ CSE_2110/src/Chapter_5/Review_Evens.java
@@ -1,12 +1,60 @@
+/*
+Program: Review_Evens.java         Last Date of this Revision: March 15, 2022
 
+
+
+Purpose: Displays the even numbers between 1 and 20 
+
+Author: Aiden Storer 
+School: CHHS
+Course: Computer Science 20
+ 
+
+*/
 
 package Chapter_5;
 
-public class Review_Evens {
+public class Review_Evens 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		//Sets num as an int
+		int num;
+		
+		//Sets num to 0
+		num = 0;
+	
+		
+		do 
+			{
+		//increases num by 2
+			 num += 2;
+			 
+		//displays num 
+			 System.out.println("\t" + num + "\t|");
+			 
+			}
+		//loops above code till num = 20
+		while(num != 20);
+		
+		//displays a line 
+		System.out.println("~~~~~~~~~~~~~~~~");
 	}
 
 }
+/* Screen Dump
+
+		2		|
+		4		|
+		6		|
+		8		|
+		10		|
+		12		|
+		14		|
+		16		|
+		18		|
+		20		|
+~~~~~~~~~~~~~~~~
+
+ */ 
