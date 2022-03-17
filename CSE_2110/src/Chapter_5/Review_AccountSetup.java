87f4c86850f14fc135151ad5dff04372c8591919
105267002 <105267002@L815WD150053.edu.cbe.ab.ca>
2022-03-17 11:03:00 -0600

    Done(March 17, 2022)

--- CSE_2110/src/Chapter_5/Review_AccountSetup.java
+++ CSE_2110/src/Chapter_5/Review_AccountSetup.java
@@ -1,10 +1,10 @@
 /*
-Program: Review_NumberSum.java         Last Date of this Revision: March 16, 2022
+Program: Review_AccountSetup.java         Last Date of this Revision: March 17, 2022 
 
 
 
 
-Purpose: Displays all numbers between 1 and the inputed maxNumthen display the sum of all numbers added together
+Purpose: Stores and outputs a username and password but it will prompt for a password till a 8 character long password is given
 
 Author: Aiden Storer 
 School: CHHS
@@ -23,27 +23,48 @@ public class Review_AccountSetup
 	{
 		String password, username;
 		
+		//Stores password as "p" to be used as a place holder
 		password = "P";
-		System.out.println("What is your username: ");
+		
+		//Prompts user for username
+		System.out.print("What is your username: ");
 		
 		//Scans for users input
 		Scanner user = new Scanner(System.in);
 		
+		//Stores users input
 		username = user.next();
 		
 		//Scans for users input
 		Scanner pass = new Scanner(System.in);
 		
+		//Repeats code below till password is 8 letters long 
 		while(password.length() < 8)
-		{
-		System.out.println("What is your password(must be 8 character): ");
-		password = pass.next();
-		}
+			{
+				//Prompts user for password
+				System.out.print("\n" + "What is your password(must be 8 character): ");
+				
+				//Stores users input
+				password = pass.next();
+			}
 		
+		//Changes username and password to lowercase
+		username = username.toLowerCase();
+		password = password.toLowerCase();
+		
+		//Prints the username and password entered
+		System.out.print("\n" + "Your username is: " + username + "\n" + "Your password is: " + password);
+
 	}
 
 }
 /* Screen Dump
+ 
+What is your username: ComputERMan
 
+What is your password(must be 8 character): THis_Is_Fun
+
+Your username is: computerman
+Your password is: this_is_fun
 
 */ 
