7408fec4a5ecf4e8413ae7214e8c754098750100
105267002 <105267002@L815WD150053.edu.cbe.ab.ca>
2022-03-18 09:45:47 -0600

    Work In Progress... (March 18, 2022) Needed: critical thinking

---
+++ CSE_2110/src/Chapter_5/Chapter_Status.java
@@ -0,0 +1,20 @@
+//Ignore this 
+//This is to change the chapter comment
+package Chapter_5;
+
+public class Chapter_Status 
+{
+
+	public static void main(String[] args) 
+	{
+		/*
+		  
+		 Work In Progress... (March 18, 2022)
+		 Needed:
+		 - critical thinking 
+		  
+		 */
+
+	}
+
+}
