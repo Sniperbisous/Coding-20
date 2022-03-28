865139f96e1bf8619962eea0df6a45a8ec6bd902
105267002 <105267002@L815WD220029.edu.cbe.ab.ca>
2022-03-28 10:21:50 -0600

    Need to change header...

--- CSE_2110/src/Chapter_6/Review_TimeConverter.java
+++ CSE_2110/src/Chapter_6/Review_TimeConverter.java
@@ -1,4 +1,20 @@
+/*
 
+Program: Grade_Exercise_5.java         Last Date of this Revision: March 8, 2022
+
+
+
+
+
+
+Purpose: To display the letter grade associated with the inputed percentage
+
+Author: Aiden Storer 
+School: CHHS
+Course: Computer Science 20
+ 
+
+*/
 
 
 package Chapter_6;
@@ -7,28 +23,40 @@ import java.util.Scanner;
 
 public class Review_TimeConverter 
 {
-
+	//Runs all following code when min_to_hour is called
 	public static int min_to_hour()
 	{
-		System.out.println("\n" + "Minutes to hours ");
+		//Prints "Minutes to hours"
+		System.out.println("\n" + "Minutes to hours");
 		System.out.println("*************************** ");
+		
+		//sets min and h as int variable
 		int min, h;
 		
 		//Scans for users input
 		Scanner user = new Scanner(System.in);
 		
+		//Prints "Input number of min: "
 		System.out.println("Input number of min: ");
+		
 		//Stores the user input
 		min = user.nextInt();
 		
+		//Calculates h from min
 		h = min / 60;
+		
+		//Returns the h variable
 		return h;
 	}
 	
+	//Runs all following code when hour_to_min is called
 	public static int hour_to_min()
 	{
+		//Prints "Hours to minutes"
 		System.out.println("\n" + "Hours to minutes");
 		System.out.println("*************************** ");
+		
+		//sets min and h as int variable
 		int min, h;
 		
 		//Scans for users input
@@ -39,15 +67,21 @@ public class Review_TimeConverter
 		//Stores the user input
 		h = user.nextInt();
 		
+		//Calculates min from h
 		min = h * 60;
+		
+		//Returns the min variable
 		return min;
 	}
 	
+	//Runs all following code when days_to_hours is called
 	public static int days_to_hours()
 	{
+		//Prints "Days to hours"
 		System.out.println("\n" + "Days to hours");
 		System.out.println("*************************** ");
 		
+		//sets day and h as int variable
 		int day, h;
 		
 		//Scans for users input
@@ -57,15 +91,21 @@ public class Review_TimeConverter
 		//Stores the user input
 		day = user.nextInt();
 		
+		//Calculates h from day
 		h = day * 24;
+		
+		//Returns the h variable
 		return h;
 	}
 	
+	//Runs all following code when hours_to_days is called
 	public static int hours_to_days()
 	{
+		//Prints "Hours to days"
 		System.out.println("\n" + "Hours to days");
 		System.out.println("*************************** ");
 		
+		//sets day and h as int variable
 		int day, h;
 		
 		//Scans for users input
@@ -75,46 +115,52 @@ public class Review_TimeConverter
 		//Stores the user input
 		h = user.nextInt();
 		
+		//Calculates day from h
 		day = h / 24;
+		
+		//Returns the day variable
 		return day;
 	}
 	
 	
 	public static void main(String[] args) 
 	{
-		int min, h, d, choice;
+		//Sets choice
+		int choice;
 		
 		//Scans for users input
 		Scanner choices = new Scanner(System.in);
 		
+		//Prints the choices along with its assigned number
 		System.out.println("1. Min to hours" + "\n2. Hours to min" + "\n3. days to hours" + "\n4. hours to days");
 		choice = choices.nextInt();
 		
 		
+		//Selects a case based on the value of the choice variable
 		switch(choice)
 		{
 		
 		case 1:
 			{
-				
+				//Prints "The approximate number of hours is" along with the resulting number of the function min_to_hour
 				System.out.println("The approximate number of hours is " + min_to_hour());
 				break;
 			}
 		case 2:
 			{
-				
+				//Prints "The approximate number of minutes is" along with the resulting number of the function hour_to_min
 				System.out.println("The approximate number of minutes is " + hour_to_min());
 				break;
 			}
 		case 3:
 			{
-				
+				//Prints "The approximate number of hours is" along with the resulting number of the function days_to_hours
 				System.out.println("The approximate number of hours is " + days_to_hours());
 				break;
 			}
 		case 4:
 			{
-				
+				//Prints "The approximate number of days is" along with the resulting number of the function hours_to_days
 				System.out.println("The approximate number of days is " + hours_to_days());
 				break;
 			}
