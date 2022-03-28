dc6619d2433f9c1658bd17cfe72d67c439e4d2ab
farme <farme@DESKTOP-PUP8SI4>
2022-03-27 23:13:08 -0600

    Work in Progress...

---
+++ CSE_2110/src/Chapter_6/Ex3_PrimeNumber.java
@@ -0,0 +1,32 @@
+/*
+
+package Chapter_6;
+
+import java.util.Scanner;
+
+public class Ex3_PrimeNumber 
+{
+	
+
+	public static void isPrime(int num)
+	{
+		do {
+			num / div 
+			
+		}while(div <= num);
+		prime =
+		num = prime;
+	}
+	
+	public static void main(String[] args)
+	{
+	//Scans for users input
+	Scanner user = new Scanner(System.in);
+		int num, prime;
+		num = user.nextInt();
+		
+
+	}
+
+}
+*/
--- CSE_2110/src/Chapter_6/Review_TimeConverter.java
+++ CSE_2110/src/Chapter_6/Review_TimeConverter.java
@@ -4,7 +4,14 @@ public class Review_TimeConverter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int sum, num;
+		sum = 0;
+		for(num = 3; num <= 10; num ++)
+		  {
+		  	sum = sum + num;
+		  	System.out.println(num);
+		  }
+		System.out.println(sum);
 	}
 
 }
