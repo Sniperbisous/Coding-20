74fabe56451c96be9937990202b25f3f5c68e15f
105267002 <105267002@L815WD220029.edu.cbe.ab.ca>
2022-03-29 11:58:51 -0600

    work in progress needs comments and header and footer

--- CSE_2110/src/Chapter_6/Ex3_PrimeNumber.java
+++ CSE_2110/src/Chapter_6/Ex3_PrimeNumber.java
@@ -7,31 +7,53 @@ import java.util.Scanner;
 public class Ex3_PrimeNumber 
 {
 	
-
-	public static void isPrime()
+	//When called Calculates whether the number is prime
+	public static Boolean isPrime(int num)
 	{
+		//Sets div, Pnum, prime and NewP as int variables
+		int  div, Pnum, prime, NewP;
+		
+		//Sets isPrime as a boolean variable
+		Boolean isPrime;
+		
+		//Gives div the value of 1
+		div = 1;
+		
+		//Gives prime the value of 0
+		prime = 0;
+		
+		//Starts a do-while loop
 		do
 		{
-		div =+ 1;
-		Remain_prime = num % div;
+			System.out.println("=========================== ");
+			Pnum = num % div;
+			System.out.println("divder " + div);
+			div += 1;
+			System.out.println("remaining result " + Pnum);
+			switch(Pnum)
+			{
+				case 0:
+				{
+					prime += 1;
+					System.out.println(" +1" );
+				}
+			}
+			System.out.println("0 remaining counter: " + prime);
+			NewP = prime;
+		} while (div <= num);
 		
-		if (prime != 0)
-		{
-			
-		}
-			
-		Pprime = 2;
-		if (Pprime == prime)
+		
+		isPrime = (NewP == 2);
+		return isPrime;
 	}
 	
 	public static void main(String[] args)
 	{
 	//Scans for users input
 	Scanner user = new Scanner(System.in);
-		int num, prime;
-		num = user.nextInt();
-		
-
+	System.out.println("enter number");
+		int num = user.nextInt();
+		System.out.println("Is it prime? " + isPrime(num));
 	}
 
 }
