4d706b5f4253efc4526e14ffe8f2d53c2c8a67ff
105267002 <105267002@L815WD220029.edu.cbe.ab.ca>
2022-03-31 11:58:10 -0600

    Done(March 31, 2022)

---
+++ CSE_2110/src/Chapter_6/Chapter_6_CRT.java
@@ -0,0 +1,32 @@
+/*
+ 2. The method body is where the code that is supposed to be run is stored, where as method declaration is where the 
+ 	method is assigned a name, parameters and type.
+ 	
+ 	
+ 3. A Access modifier is used.
+ 
+ 4. A public method or a Static method. (public and static)
+ 
+ 5. var1: limited to the main function
+ 	var2: limited to the main function 
+ 	var3: limited to method 1
+ 	var4: limited to method 1
+ 	
+ 6.	
+ 	A) public static int getVowels(string vowel)
+		{
+		}
+		
+ 	B) public static int extractDigit(int num)
+		{
+		}
+		
+	C) public static string insertString(string name, int num)
+		{
+			return (name);
+		}
+		
+7. 
+	A) each method has its own assigned name.
+	B) no because when the function is called it wont be able to know which function to run leading to an error 
+*/
