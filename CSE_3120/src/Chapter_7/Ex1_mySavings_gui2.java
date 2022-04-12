b42205bb2c9b5d13a34ef3360b19592f84427fce
105267002 <105267002@L815WD220029.edu.cbe.ab.ca>
2022-04-12 11:01:13 -0600

    Working on

--- CSE_3120/src/Chapter_7/Ex1_mySavings_gui2.java
+++ CSE_3120/src/Chapter_7/Ex1_mySavings_gui2.java
@@ -5,6 +5,7 @@ import java.awt.BorderLayout;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
@@ -34,13 +35,13 @@ public class Ex1_mySavings_gui2
 		JButton newButton = new JButton("Submit");
 		newButton.setSize(100, 100);
 		
-		JTextArea textArea = new JTextArea("hahaha");
-		textArea.setSize(30, 30);
+		JLabel textLabel = new JLabel("Quarters");
+		textLabel.setSize(100, 30);
 		JPanel JP1 = panel1;
 		
 		JP1.add(newButton, BorderLayout.EAST);
-		JP1.add(textArea, BorderLayout.WEST);
-		main_frame.add(JP1);
+		JP1.add(textLabel, BorderLayout.WEST);
+		main_frame.add(JP1, BorderLayout.NORTH);
 		return (main_frame);
 		
 	}
@@ -50,20 +51,44 @@ public class Ex1_mySavings_gui2
 		JPanel panel2 = new JPanel();
 		panel2.setLayout(borderLayoutManager);
 		
-		BoxLayout boxLayoutManager = new BoxLayout(panel2, BoxLayout.Y_AXIS);
-		panel2.setLayout(boxLayoutManager);
+		//BoxLayout boxLayoutManager = new BoxLayout(panel2, BoxLayout.Y_AXIS);
+		//panel2.setLayout(boxLayoutManager);
 		
 		JButton newButton = new JButton("Submit");
 		newButton.setSize(100, 100);
 		
-		JTextArea textArea = new JTextArea("hahaha");
-		textArea.setSize(30, 30);
+		JLabel textLabel = new JLabel("Dimes");
+		textLabel.setSize(100, 30);
 		
 		JPanel JP2 = panel2;
 		
 		JP2.add(newButton, BorderLayout.EAST);
-		JP2.add(textArea, BorderLayout.WEST);
+		JP2.add(textLabel, BorderLayout.WEST);
 		main_frame.add(JP2);
+		return (main_frame);
+		
+	}
+	
+	public JFrame addNickels()
+	{
+		BorderLayout borderLayoutManager = new BorderLayout();
+		JPanel panel3 = new JPanel();
+		panel3.setLayout(borderLayoutManager);
+		
+		//BoxLayout boxLayoutManager = new BoxLayout(panel2, BoxLayout.Y_AXIS);
+		//panel2.setLayout(boxLayoutManager);
+		
+		JButton newButton = new JButton("Submit");
+		newButton.setSize(100, 100);
+		
+		JLabel textLabel = new JLabel("Dimes");
+		textLabel.setSize(100, 30);
+		
+		JPanel JP3 = panel3;
+		
+		JP3.add(newButton, BorderLayout.EAST);
+		JP3.add(textLabel, BorderLayout.WEST);
+		main_frame.add(JP3);
 		return (main_frame);
 		
 	}
@@ -73,6 +98,7 @@ public class Ex1_mySavings_gui2
 		Ex1_mySavings_gui2 main_gui = new Ex1_mySavings_gui2();
 		main_gui.addQuarters();
 		main_gui.addDimes();
+		main_gui.addNickels();
 		
 		System.out.print(main_gui);
 		main_frame.setVisible(true);
