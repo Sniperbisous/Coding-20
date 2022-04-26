75e847bc507e2b42fa6f9b894460c9ca896b0c10
105267002 <105267002@L815WD220029.edu.cbe.ab.ca>
2022-04-26 11:59:35 -0600

    ALMOST DONE JUST more crazy

--- CSE_3120/src/Chapter_7/Ex8_Adder_Functions.java
+++ CSE_3120/src/Chapter_7/Ex8_Adder_Functions.java
@@ -4,33 +4,42 @@ public class Ex8_Adder_Functions
 	{
 	private int total_correct;
 	private int attempt;
-	private int rand_n1, rand_n2;
-	
+	private int rand_n1; 
+	private int rand_n2;
 		public  Ex8_Adder_Functions()
 		{
 			total_correct = 0;
 		}
 		
-		public int New_num_1()
+		public int get_New_num_1()
 		{
 			int rand_n1;
 			rand_n1 = (int)(20 * Math.random() + 0.04); 
 			return(rand_n1);
 		}
 		
-		public int New_num_2()
+		public int get_New_num_2()
 		{
 			
 			rand_n2 = (int)(20 * Math.random() + 0.04); 
 			return(rand_n2);
 		}
 		
-		public Boolean answer(int f, int s, int a)
+		public String get_answer(int f, int s, int a)
 		{
+			String dis;
 			int answer = (f + s);
 			boolean true_false = (a == answer); 
+			if (a == answer)
+			{
+				dis = "Your answer is true. Here is another question.";
+			}
 			
-			return (true_false);
+			else 
+			{
+				dis = "Your answer is false. Enter another answer.";
+			}
+			return (dis);
 			
 		}
 		
@@ -42,21 +51,30 @@ public class Ex8_Adder_Functions
 			{
 				case 1: 
 				{
-					total_correct =+ 5;
+					total_correct += 5;
+					break;
 				}
 				
 				case 2: 
 				{
-					total_correct =+ 3;
+					total_correct += 3;
+					break;
 				}
 				
 				case 3: 
 				{
-					total_correct =+ 1;
+					total_correct += 1;
+					break;
 				}
+				case 4: 
+				{
+					total_correct += 0;
+					break;
+				}
+					
 			}
 		}
-		public int Total()
+		public int get_Total()
 		{
 			return (total_correct);
 			
--- CSE_3120/src/Chapter_7/Ex8_Adder_main.java
+++ CSE_3120/src/Chapter_7/Ex8_Adder_main.java
@@ -7,31 +7,49 @@ public class Ex8_Adder_main
 
 	public static void main(String[] args) 
 	{
-		int awnser, rand_n1, rand_n2, input_awn, n1, n2;
+		//Scans for users input
+		Scanner user = new Scanner(System.in);
 		
+		int awnser, rand_n1, rand_n2, input_awn, n1, n2, attempts;
+		//input_awn = 0;
 		awnser = 0;
 		Ex8_Adder_Functions Attempt = new Ex8_Adder_Functions();
+		Ex8_Adder_Functions Attempt1 = new Ex8_Adder_Functions();
 		
 		  do 
 		{
-			 //Scans for users input
-			 Scanner user = new Scanner(System.in);
+			 attempts = 0;
 			 
-			 n1 = Attempt.New_num_1(); 
-			 n2 = Attempt.New_num_2(); 
+			 n1 = Attempt.get_New_num_1(); 
+			 n2 = Attempt.get_New_num_2(); 
 			 
-			 System.out.print(n1 + " + " + n2 + " = ");
+			   
+			  do{
+				  attempts += 1;
+				  System.out.print(n1 + " + " + n2 + " = ");
 			
-			 //Stores users input
-			input_awn = user.nextInt();
+				  //Stores users input
+				  input_awn = user.nextInt();
+			 
 			
-			System.out.println("your answer is " + Attempt.answer(n1, n2, input_awn));
+				  System.out.println("your answer is " + Attempt.get_answer(n1, n2, input_awn));
+				  
+				  System.out.println("test:attempts " + attempts);
+				  
+			  }while (input_awn != (n1 + n2) && input_awn != 999 && attempts < 3);
 			
+			  if(Attempt.get_answer(n1, n2, input_awn) != Attempt.get_answer(1, 1, 2))
+			  {
+				  attempts += 1;
+			  }
+			  
+			  
+			Attempt.Attempt(attempts);
 			
-			
+			System.out.println(Attempt.get_Total());
 			
 		}
-		while(awnser != 999);
+		while(input_awn != 999);
 		
 		
 
