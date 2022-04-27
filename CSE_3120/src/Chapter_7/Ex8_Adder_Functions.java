90bba380af76df0c8e1f6f1b57c9d8ea85f68772
105267002 <105267002@L815WD220029.edu.cbe.ab.ca>
2022-04-27 10:19:22 -0600

    Done

--- CSE_3120/src/Chapter_7/Ex8_Adder_Functions.java
+++ CSE_3120/src/Chapter_7/Ex8_Adder_Functions.java
@@ -2,34 +2,37 @@ package Chapter_7;
 
 public class Ex8_Adder_Functions 
 	{
-	private int total_correct;
+	private int total_Points;
 	private int attempt;
 	private int rand_n1; 
 	private int rand_n2;
+	
+		//Sets the amount of total_correct to 0 when called
 		public  Ex8_Adder_Functions()
 		{
-			total_correct = 0;
+			total_Points = 0;
 		}
 		
+		//Provides a random number between 0-20 when called
 		public int get_New_num_1()
 		{
-			int rand_n1;
 			rand_n1 = (int)(20 * Math.random() + 0.04); 
 			return(rand_n1);
 		}
 		
+		//Provides a random number between 0-20 when called
 		public int get_New_num_2()
 		{
-			
 			rand_n2 = (int)(20 * Math.random() + 0.04); 
 			return(rand_n2);
 		}
 		
+		//Returns 1 of 2 possible strings depending on the inputed values
 		public String get_answer(int f, int s, int a)
 		{
 			String dis;
 			int answer = (f + s);
-			boolean true_false = (a == answer); 
+			
 			if (a == answer)
 			{
 				dis = "Your answer is true. Here is another question.";
@@ -40,10 +43,10 @@ public class Ex8_Adder_Functions
 				dis = "Your answer is false. Enter another answer.";
 			}
 			return (dis);
-			
 		}
 		
-		public void Attempt(int a)
+		//Sets the amount of points earned based on the inputed amount of attempts
+		public void set_points(int a)
 		{
 			attempt = a;
 			
@@ -51,32 +54,34 @@ public class Ex8_Adder_Functions
 			{
 				case 1: 
 				{
-					total_correct += 5;
+					total_Points += 5;
 					break;
 				}
 				
 				case 2: 
 				{
-					total_correct += 3;
+					total_Points += 3;
 					break;
 				}
 				
 				case 3: 
 				{
-					total_correct += 1;
+					total_Points += 1;
 					break;
 				}
 				case 4: 
 				{
-					total_correct += 0;
+					total_Points += 0;
 					break;
 				}
 					
 			}
 		}
+		
+		//Returns the total_correct value
 		public int get_Total()
 		{
-			return (total_correct);
+			return (total_Points);
 			
 		}
 	}
--- CSE_3120/src/Chapter_7/Ex8_Adder_main.java
+++ CSE_3120/src/Chapter_7/Ex8_Adder_main.java
@@ -1,3 +1,18 @@
+/*
+Program: Review_Circle_Part4.java         Last Date of this Revision: April 7, 2022 
+
+
+
+
+Purpose: To overwrite the original input for the functions equals and toString from Circle.java 
+
+Author: Aiden Storer 
+School: CHHS
+Course: Computer Science 20
+ 
+
+*/
+
 package Chapter_7;
 
 import java.util.Scanner;
@@ -10,50 +25,125 @@ public class Ex8_Adder_main
 		//Scans for users input
 		Scanner user = new Scanner(System.in);
 		
-		int awnser, rand_n1, rand_n2, input_awn, n1, n2, attempts;
-		//input_awn = 0;
-		awnser = 0;
-		Ex8_Adder_Functions Attempt = new Ex8_Adder_Functions();
-		Ex8_Adder_Functions Attempt1 = new Ex8_Adder_Functions();
+		//sets rand_n1, rand_n2, input_awn, n1, n2 and attempts as int variables
+		int rand_n1, rand_n2, input_awn, n1, n2, attempts;
 		
-		  do 
-		{
-			 attempts = 0;
+		//Creates two objects of Ex8_Adder_Functions.java
+		Ex8_Adder_Functions Attempt1 = new Ex8_Adder_Functions();
+		Ex8_Adder_Functions Attempt2 = new Ex8_Adder_Functions();
+		
+		//Loops the following code till input_awn = 999 
+		do {
+			
+			//Initializes the variable attempts.
+			attempts = 0;
 			 
-			 n1 = Attempt.get_New_num_1(); 
-			 n2 = Attempt.get_New_num_2(); 
+			//Gets two new numbers.
+			n1 = Attempt1.get_New_num_1(); 
+			n2 = Attempt1.get_New_num_2(); 
 			 
-			   
+			  //Loops the following code till input_awn = the correct answer or input_awn = 999 or attempt >= 3.
 			  do{
+				 
+				  //Adds 1 to the attempt variable.
 				  attempts += 1;
+				  
+				  //Displays the question.
+				  System.out.println("Question: ");
 				  System.out.print(n1 + " + " + n2 + " = ");
 			
 				  //Stores users input
 				  input_awn = user.nextInt();
-			 
-			
-				  System.out.println("your answer is " + Attempt.get_answer(n1, n2, input_awn));
 				  
-				  System.out.println("test:attempts " + attempts);
+				  //Executes the following code if input_awn does not = 999
+				  if (input_awn != 999)
+				  {
+					  //Runs the function get_answer from the Ex8_Adder_Functions.java class
+					  System.out.println(Attempt1.get_answer(n1, n2, input_awn));
+					  System.out.println("Number of attempts: " + attempts); 
+				  }	 	 
+				  
+				  
+				  else 
+				  {
+					  //Displays "Thanks for playing!"
+					  System.out.println("\n_____________________________\n ");
+					  System.out.println("Thanks for playing!");
+				  }
+				  
+				  
+				  System.out.println("\n_____________________________\n ");
 				  
 			  }while (input_awn != (n1 + n2) && input_awn != 999 && attempts < 3);
 			
-			  if(Attempt.get_answer(n1, n2, input_awn) != Attempt.get_answer(1, 1, 2))
+			  
+			 
+			  //Executes the following code object 1 and object 2 have the same return from the function get_answer or if input_awn does not = 999
+			  if(Attempt1.get_answer(n1, n2, input_awn) != Attempt2.get_answer(1, 1, 2) && input_awn != 999)
 			  {
+				  //Adds 1 to attempts 
 				  attempts += 1;
+				  //Displays the following code
+				  System.out.println("Oops out of tries. The correct answer was " + (n1 + n2) + ".\nHere's a new question.");
 			  }
 			  
+			  else
+			  {
+				  attempts += 0;
+			  }
 			  
-			Attempt.Attempt(attempts);
+			  //Determines the amount of points earned
+			  Attempt1.set_points(attempts);
+			  
+			  //Displays the following code providing the user with the amount of points that they earned. 
+			  System.out.println("You have a total of " + Attempt1.get_Total() + " points." );  
+			  System.out.println("\n===========================\n ");
 			
-			System.out.println(Attempt.get_Total());
+			
 			
 		}
 		while(input_awn != 999);
-		
-		
-
-
 	}
 
 }
+/* Screen Dump
+
+Question: 
+7 + 9 = 16
+Your answer is true. Here is another question.
+Number of attempts: 1
+
+_____________________________
+ 
+You have a total of 5 points.
+
+===========================
+ 
+Question: 
+13 + 14 = 9
+Your answer is false. Enter another answer.
+Number of attempts: 1
+
+_____________________________
+ 
+Question: 
+13 + 14 = 2
+Your answer is false. Enter another answer.
+Number of attempts: 2
+
+_____________________________
+ 
+Question: 
+13 + 14 = 3
+Your answer is false. Enter another answer.
+Number of attempts: 3
+
+_____________________________
+ 
+Oops out of tries. The correct answer was 27.
+Here's a new question.
+You have a total of 5 points.
+
+===========================
+
+*/
