09528d4d49d9b822fd4d34576f7b642a46150fde
105267002 <105267002@L815WD220029.edu.cbe.ab.ca>
2022-04-28 11:50:40 -0600

    Done(April 28, 2022)

---
+++ CSE_3120/src/Chapter_7/Chapter_7_CRT.java
@@ -0,0 +1,21 @@
+/*
+1. The variable of pi and radius are encapsulated.
+
+2. The same name as the class.
+
+3. The private modifier restricts it to its current class while the public should allow you to be accessed from any other class.
+
+4. If the variable is public then yes this would be valid as you can interact with public variables in other classes but if it was private than it wouldn't be valid.
+
+5.
+	a) Roo
+	b) x
+	c) getX, Roo
+	d) calculate(), setX(int z)
+	e) factor()
+	f) Roo
+	g) 4
+	
+6. A class is a set code, an object is a copy of a class that can be changed and used in another class.
+
+*/
