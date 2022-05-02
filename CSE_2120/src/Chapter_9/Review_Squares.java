24e0a1b7b6ce337396d10cef2a34af8184963ee7
105267002 <105267002@L815WD220029.edu.cbe.ab.ca>
2022-05-02 10:23:01 -0600

    start

---
+++ CSE_2120/src/Chapter_9/Review_Squares.java
@@ -0,0 +1,14 @@
+package Chapter_9;
+
+public class Review_Squares 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		
+		
+
+	}
+
+}
--- CSE_2120/src/Chapter_9/Review_Student_roster.java
+++ CSE_2120/src/Chapter_9/Review_Student_roster.java
@@ -1,9 +1,22 @@
 package Chapter_9;
 
+import java.util.Scanner;
+
 public class Review_Student_roster {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		//Scans for users input 
+		Scanner user = new Scanner(System.in);
+		int num_student = user.nextInt();
+		String[] roster = {"name"};
+		for (int i = 1; i <= num_student; i++)
+		{
+			String name = user.next();
+			roster[i] = name;
+		}
+		
+		
 
 	}
 
