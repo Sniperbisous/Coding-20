69981e92279eb781b9598fbe46813f5b5539e0fb
105267002 <105267002@L815WD220029.edu.cbe.ab.ca>
2022-05-05 11:58:46 -0600

    Add comments

--- CSE_2120/src/Chapter_9/Review_Squares.java
+++ CSE_2120/src/Chapter_9/Review_Squares.java
@@ -5,8 +5,14 @@ public class Review_Squares
 
 	public static void main(String[] args) 
 	{
+		int[] squares = new int[5];
 		
-		
+		for (int i = 0; i < 5; i++)
+		{
+			squares[i] = (i * i);
+			int element = i + 1;
+			System.out.println("element " + element + " = " + squares[i]);
+		}
 		
 
 	}
