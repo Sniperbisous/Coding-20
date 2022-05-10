5c016c5bbed93ea991df82a3f49aeb15077967f7
105267002 <105267002@L815WD220029.edu.cbe.ab.ca>
2022-05-10 11:22:49 -0600

    Done(May 10, 2022)

--- CSE_2120/src/Chapter_9/Review_Reverse_Functions.java
+++ CSE_2120/src/Chapter_9/Review_Reverse_Functions.java
@@ -1,3 +1,16 @@
+/*
+Program: Review_Reverse_Functions.java         Last Date of this Revision: May 10, 2022   
+
+
+
+
+Purpose: To create, set and return values of the array Count to the class Review_Reverse_GUI.java
+
+Author: Aiden Storer 
+School: CHHS
+Course: Computer Science 20
+ 
+*/
 package Chapter_9;
 
 public class Review_Reverse_Functions 
@@ -5,31 +18,32 @@ public class Review_Reverse_Functions
 	
 private int[] Count = new int[10];
 	
+	//Creates a constructor to set all the values of the count array
 	public Review_Reverse_Functions()
 	{
+		//loops and sets all element values in the Count array
 		for(int i = 0; i < 10; i++)
 		{
 			Count[i] = i;
 		}
 	}
 	
-	public int count_num(int i)
-	{
-		return Count[i];
-	}
-	
+	//Returns the arrays values in a reverse order as a string 
 	public String toString() 
 	{
+		//Initializes the Count_down variable 
 		String Count_down = " ";
-		String Next_Count = "";
+		
+		//Starts a loop to determine the values of the array from back to front
 		for(int i = 9; i >= 0; i--)
 		{
+			//Adds each value to the string 
 			Count_down = (Count_down + Count[i] + "  ");
-			//Next_Count = Count_down;
+			
 		}
 		
+		//Returns the final string of all the numbers
 		return Count_down;
-		
 	}
 	
 	public static void main(String[] args) 
--- CSE_2120/src/Chapter_9/Review_Reverse_GUI.java
+++ CSE_2120/src/Chapter_9/Review_Reverse_GUI.java
@@ -1,3 +1,16 @@
+/*
+Program: Review_Reverse_GUI.java         Last Date of this Revision: May 10, 2022   
+
+
+
+
+Purpose: To create a GUI to display the string returned from toString() in Review_Reverse_GUI.java. 
+
+Author: Aiden Storer 
+School: CHHS
+Course: Computer Science 20
+ 
+*/
 package Chapter_9;
 
 import java.awt.EventQueue; 
@@ -39,33 +52,41 @@ public class Review_Reverse_GUI {
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	private void initialize() 
+	{
+		//Creates a new frame
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+		//Creates a new panel
 		JPanel panel = new JPanel();
 		panel.setBounds(10, 11, 414, 239);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
+		//Creates a new label
 		JLabel lblNewLabel = new JLabel("Count Down:");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		lblNewLabel.setBounds(10, 11, 154, 53);
 		panel.add(lblNewLabel);
 		
+		//Creates a new label
 		JLabel Dis = new JLabel("");
 		Dis.setFont(new Font("Rockwell", Font.BOLD, 20));
 		Dis.setBounds(20, 59, 384, 131);
 		panel.add(Dis);
 		
+		//Creates a new button
 		JButton Count_down_button = new JButton("Display Count Down");
 		Count_down_button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) 
 			{
+				//Creates an object to pull outputs from Review_Reverse_Function.java
 				Review_Reverse_Functions object = new Review_Reverse_Functions();
 				
+				//Displays the returned string from toString() in Review_Reverse_Function.java
 				Dis.setText(object.toString());
 			}
 		});
