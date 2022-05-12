c5ca0d39c6eda8f57c0c4aee3ba02e9d71f1c696
105267002 <105267002@L815WD220029.edu.cbe.ab.ca>
2022-05-12 11:09:48 -0600

    Done(May 12, 2022)

---
+++ CSE_2120/src/Chapter_9/Chapter_9_CRT.java
@@ -0,0 +1,16 @@
+/*
+1. 2
+
+2. int[] quantities = new int[20]
+
+3. double[] heights = {1.65, 2.15, 4.95}
+
+4. 
+	int[] grades = new int[amount];
+	
+	for (int i : grades) 
+		{
+  			System.out.println(i);
+		}
+
+*/
