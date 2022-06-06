53132dfd565be46b4333d2522c5b06f3f14ceca7
farme <farme@DESKTOP-PUP8SI4>
2022-06-05 21:46:46 -0600

    working on it

---
+++ CSE_2920_Phidgets/src/Phidget/Advanced_lesson_Closing_Phidgets.java
@@ -0,0 +1,24 @@
+package Phidget;
+
+//Add Phidgets Library
+import com.phidget22.*;
+
+public class Advanced_lesson_Closing_Phidgets {
+
+	public static void main(String[] args) throws Exception
+	{
+        //Create
+        TemperatureSensor temperatureSensor = new TemperatureSensor();
+
+        //Open
+        temperatureSensor.open(1000);
+
+        //Use your Phidgets
+        System.out.println("Temperature: " + temperatureSensor.getTemperature() + " °C" );
+        
+        //Close your Phidgets
+        temperatureSensor.close();
+    }
+}
+  
+	
